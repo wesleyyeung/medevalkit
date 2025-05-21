8ba3b0e48d2998831817713ff13d5abae9b6740c
Wesley Yeung <Wesley@Ws-MacBook-Pro.local>
2025-05-21 17:59:27 +0200

    correct typos and update requirements

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import os
 from setuptools import setup, find_packages
 
-# Optional: dynamically read README
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='evalkit',
     version='0.1.0',
-    description='A toolkit for evaluating machine learning models.',
+    description='A toolkit for evaluating machine learning models for healthcare applications.',
     long_description=read("README.md") if os.path.exists("README.md") else "",
     long_description_content_type="text/markdown",
     author='wy',
-    author_email='author_email',
+    author_email='corona-monody-7e@icloud.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'numpy',
-        'scikit-learn',
+        'lifelines',
         'matplotlib',
-        'seaborn',
-        'xgboost'
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'scikit-survival',
+        'seaborn'
     ],
     python_requires='>=3.7',
 )
